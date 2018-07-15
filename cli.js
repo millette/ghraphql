97229201d769150f815054237ac65607ec91ac04
Robin Millette <robin@millette.info>
2018-07-15 02:08:46 -0400

    cli now accepts arguments.

--- cli.js
+++ cli.js
@@ -3,6 +3,13 @@
 // self
 const graphqlGot = require('.')
 
-graphqlGot('kinshasa')
-  .then(body => console.log(JSON.stringify(body, null, '  ')))
-  .catch(console.error)
+const where = process.argv.slice(2)
+if (where.length) {
+  graphqlGot(where)
+    .then(body => console.log(JSON.stringify(body, null, '  ')))
+    .catch(console.error)
+} else {
+  console.error(
+    'Required location argument. Prefer "montréal" to "montreal". You can pass many locations, use quotes "" or escape spaces.'
+  )
+}
