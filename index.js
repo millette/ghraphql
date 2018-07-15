0c455fd0dff1a66790981d40da566eedb8030a9e
Robin Millette <robin@millette.info>
2018-07-15 05:51:25 -0400

    Fix relative default query file path.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // core
 const { readFileSync } = require('fs')
+const { join } = require('path')
 
 // npm
 const got = require('got')
@@ -45,7 +46,7 @@ const makeSearch = where =>
     .map(x => `location:${JSON.stringify(x)}`)
     .join(' ')} sort:joined`
 
-const defaultQuery = readFileSync('query.graphql', 'utf-8')
+const defaultQuery = readFileSync(join(__dirname, 'query.graphql'), 'utf-8')
 
 const graphqlGot = async (where, query) => {
   try {
