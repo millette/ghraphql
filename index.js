64f9050d1e8cdfca5c7cca5475830f2c8f3614fc
Robin Millette <robin@millette.info>
2018-08-05 15:54:37 -0400

    Use got 9, success.

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const processor = require('./processor')
 
 // npm
 const got = require('got')
-const pRetry = require('p-retry')
 const deburr = require('lodash.deburr')
 const uniq = require('lodash.uniq')
 const uniqBy = require('lodash.uniqby')
@@ -21,10 +20,15 @@ const delay = require('delay')
 const debug = require('debug')(name)
 const bestContrast = require('get-best-contrast-color').default
 
-const MIN_WAIT = 50
+// const MIN_WAIT = 50
 
 const GOT_OPTS = {
-  retries: 0,
+  // default: throwHttpErrors: true, // current run: false (by mistake I believe)
+  retry: {
+    retries: 5,
+    methods: ['POST'],
+    statusCodes: [403, 408, 413, 429, 502, 503, 504]
+  },
   json: true,
   headers: {
     authorization: `bearer ${process.env.GITHUB_TOKEN}`,
@@ -33,32 +37,6 @@ const GOT_OPTS = {
 }
 
 const WHERE_ERROR = '"where" argument should be a string or an array.'
-
-let tickerWarn = () => false
-// f  1    2   3   4     5   6    7
-// 4  15s, 1m, 4m, 16m, 64m
-// 3  7,   20, 1m, 3m,   9m, 27m, 81m
-const RETRY_OPTS = {
-  retries: 8,
-  factor: 3,
-  minTimeout: 20000,
-  maxTimeout: 65 * 60 * 1000,
-  onFailedAttempt: error => {
-    tickerWarn(`${new Date().toISOString()} ${error.toString()}`)
-    tickerWarn(
-      `${new Date().toISOString()} Attempt ${
-        error.attemptNumber
-      } failed. There are ${error.attemptsLeft} attempts left.`
-    )
-
-    debug(error.toString())
-    debug(
-      `Attempt ${error.attemptNumber} failed. There are ${
-        error.attemptsLeft
-      } attempts left.`
-    )
-  }
-}
 
 const localFile = path => readFileSync(join(__dirname, path), 'utf-8')
 
@@ -108,24 +86,15 @@ const makeSearch = (where, created) =>
 
 const defaultQuery = localFile('query.graphql')
 
-const gotRetry = async (query, variables) => {
-  const gotRun = () =>
-    got('https://api.github.com/graphql', {
-      ...GOT_OPTS,
-      body: { query, variables }
-    })
-      .then(ret => {
-        debug(ret.headers)
-        debug(Object.keys(ret.body))
-        return ret
-      })
-      .catch(error => {
-        debug('ERROR', error)
-        throw error.statusCode === 401 ? new pRetry.AbortError(error) : error
-      })
-
-  return pRetry(gotRun, RETRY_OPTS)
-}
+const gotRetry = (query, variables) =>
+  got('https://api.github.com/graphql', {
+    ...GOT_OPTS,
+    body: { query, variables }
+  }).then(ret => {
+    debug(ret.headers)
+    debug(Object.keys(ret.body))
+    return ret
+  })
 
 const graphqlGotImp = async (where, query, variables = {}) => {
   try {
@@ -187,6 +156,7 @@ const graphqlGotImp = async (where, query, variables = {}) => {
         })
         : []
 
+    data._headers = headers
     return data
   } catch (e) {
     throw e
@@ -195,39 +165,51 @@ const graphqlGotImp = async (where, query, variables = {}) => {
 
 const throttle = async (then, userCount, nPerQuery, rateLimit) => {
   const now = Date.now()
-  const remaining = rateLimit.remaining || rateLimit.limit
+  const remaining = rateLimit.remaining // || rateLimit.limit
   const elapsed = now - then
   debug('elapsed, rateLimit:', elapsed, rateLimit)
-  const ttt = Math.round(
-    (Date.parse(rateLimit.resetAt) - now) /
-      Math.round(remaining / rateLimit.cost)
-  )
-
+  const timeUntilReset = Math.max(0, rateLimit.resetAt - now)
   const nQueries = Math.round(userCount / nPerQuery)
-  const timeNeeded = Math.round(nQueries * ttt)
-  const timeUntilReset = Date.parse(rateLimit.resetAt) - now
   const costNeeded = rateLimit.cost * nQueries
+
+  debug('nQueries:', nQueries)
+  debug('timeUntilReset:', timeUntilReset)
+  debug('costNeeded:', costNeeded)
 
   let ms
 
-  if (rateLimit.cost > remaining) {
-    ms = timeUntilReset + 20 * MIN_WAIT
-  } else if (timeNeeded > timeUntilReset && costNeeded > remaining) {
-    ms = Math.max(MIN_WAIT, Math.round(1.25 * (ttt - Math.round(elapsed))))
+  if (!remaining || rateLimit.cost > remaining) {
+    // ms = timeUntilReset + 20 * MIN_WAIT
+    ms = timeUntilReset + 30 * 1000
   } else {
-    ms = MIN_WAIT
+    const ttt = Math.round(
+      timeUntilReset / Math.round(remaining / rateLimit.cost)
+    )
+
+    const timeNeeded = Math.round(nQueries * ttt)
+    debug('timeNeeded:', timeNeeded)
+
+    if (timeNeeded > timeUntilReset && costNeeded > remaining) {
+      ms = Math.max(0, Math.round(ttt - Math.round(elapsed)))
+    }
   }
 
-  debug('nQueries:', nQueries)
-  debug('timeNeeded:', timeNeeded)
-  debug('timeUntilReset:', timeUntilReset)
-  debug('costNeeded:', costNeeded)
+  /*
+  } else if (timeNeeded > timeUntilReset && costNeeded > remaining) {
+    // ms = Math.max(MIN_WAIT, Math.round(1.25 * (ttt - Math.round(elapsed))))
+    // ms = Math.max(1000, Math.round(1.25 * (ttt - Math.round(elapsed))))
+    // ms = Math.max(1000, ttt)
+    // ms = ttt
+    ms = Math.max(0, Math.round(ttt - Math.round(elapsed)))
+  }
+  */
   debug('ms:', ms)
 
-  await delay(ms)
-
-  if (process.env.DEBUG === name) {
+  if (process.env.DEBUG === name || process.env.DEBUG === '*') {
     console.error()
+  }
+  if (ms) {
+    await delay(ms)
   }
 }
 
@@ -238,6 +220,7 @@ const graphqlGot = async (where, query, variables = {}, tick = false) => {
   let data
   let result = []
   let lastCreated
+  let prev
   try {
     let after = false
     let created
@@ -249,13 +232,6 @@ const graphqlGot = async (where, query, variables = {}, tick = false) => {
       const then = Date.now()
       data = await graphqlGotImp(where, query, { ...variables, after, created })
 
-      if (data.search && data.search.edges && data.search.edges.length) {
-        lastCreated =
-          data.search.edges[data.search.edges.length - 1].node.createdAt
-      }
-
-      debug('lastCreated:', lastCreated)
-
       r2 = result.length
       result = uniqBy(result.concat(data.search.edges), 'node.databaseId')
 
@@ -265,7 +241,6 @@ const graphqlGot = async (where, query, variables = {}, tick = false) => {
 
       if (tick) {
         if (first) {
-          tickerWarn = warn => tick(0, { warn })
           tick(result.length - r2, { total: userCount })
           first = false
         } else {
@@ -275,18 +250,30 @@ const graphqlGot = async (where, query, variables = {}, tick = false) => {
 
       debug('result.length, userCount:', result.length, userCount)
       after = data.search.pageInfo.hasNextPage && data.search.pageInfo.endCursor
-      if (after) {
-        await throttle(
-          then,
-          userCount - result.length,
-          data.search.edges.length,
-          data.rateLimit
-        )
-      } else {
-        created = result.length < userCount && lastCreated
-        debug('created', created)
+      if (!after) {
+        prev = lastCreated
+        debug('lastCreated (old)', prev)
+        lastCreated = result[result.length - 1].node.createdAt
+        debug('result.length', result.length)
+        debug('result[last]', result[result.length - 1])
+        debug('userCount', userCount)
+        created =
+          lastCreated !== prev && result.length < userCount && lastCreated
+        debug('lastCreated (new)', lastCreated)
         debug('created', created)
       }
+      await throttle(
+        then,
+        userCount - result.length,
+        data.search.edges.length,
+        {
+          cost: data.rateLimit.cost,
+          limit: parseInt(data._headers['x-ratelimit-limit'], 10),
+          remaining: parseInt(data._headers['x-ratelimit-remaining'], 10),
+          resetAt: 1000 * parseInt(data._headers['x-ratelimit-reset'], 10)
+        }
+        // data.rateLimit
+      )
     } while (after || created)
 
     if (result.length) {
@@ -300,6 +287,26 @@ const graphqlGot = async (where, query, variables = {}, tick = false) => {
     debug('FIXME (statusCode) ?', e.statusCode)
     debug('lastCreated:', lastCreated)
     debug(e)
+
+    /*
+    if (
+      result &&
+      result.length
+    ) {
+      if (
+        data &&
+        data.search &&
+        data.search.edges &&
+        data.search.edges.length
+      ) {
+        debug(Object.keys(data))
+        data.search.edges = result
+        return data
+      }
+      // return []
+    }
+    */
+    /*
     if (
       result &&
       result.length &&
@@ -312,6 +319,7 @@ const graphqlGot = async (where, query, variables = {}, tick = false) => {
       data.search.edges = result
       return data
     }
+    */
     throw e
   }
 }
